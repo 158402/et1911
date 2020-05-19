723e28bb281945a0d1535c023783d251c4f19248
xuechangheng <xuechangheng@qq.com>
2020-05-19 10:26:51 +0800

    提交hell/lx

---
+++ spring-01-lx/src/main/java/com/etoak/bean/Belle.java
@@ -0,0 +1,20 @@
+package com.etoak.bean;
+
+public class Belle {
+	
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	@Override
+	public String toString() {
+		return "Belle [name=" + name + "]";
+	}
+	
+	
+
+}
---
+++ spring-01-lx/src/main/java/com/etoak/controller/LoveController.java
@@ -0,0 +1,32 @@
+package com.etoak.controller;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.util.StringUtils;
+import org.springframework.web.HttpRequestHandler;
+public class LoveController implements HttpRequestHandler {
+
+	@Override
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		//接收参数
+		String name = request.getParameter("name");
+		
+		System.out.println(name);
+		
+		//向request域中赋值
+		if(StringUtils.isEmpty(name)){
+			name = "World";
+		}
+		
+		//调用service 向request传值
+		request.setAttribute("result", "Hello" + name);
+		//返回页面(webapp/Love.jsp)
+		request.getRequestDispatcher("/Love.jsp").forward(request, response);
+		
+
+	}
+
+}
